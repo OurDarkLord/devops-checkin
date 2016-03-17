a78f525e9c8942b4fdd006760de9b497ed28bcae
Tim Asscherickx <tim.asscherickx@hotmail.com>
2016-03-17 11:31:31 +0100

    error pages werken

--- express.js
+++ express.js
@@ -16,6 +16,31 @@ app.get('/', function(req,res){
 	res.render('home');
 });
 
+app.use(function(req,res,next){
+	console.log("looking for URL:" + req.url);
+	next();
+});
+
+
+
+app.get('/about', function(req,res){
+	res.render('about');
+
+});
+
+app.use(function(req, res){
+	res.type('text/html');
+	res.status(404);
+	res.render('404');
+});
+
+app.use(function(err, req,res,next){
+	console.error(err.stack);
+	res.status(500);
+	res.render('500');
+});
+
+
 app.listen(app.get('port'),function(){
 	console.log('Express started')
 });

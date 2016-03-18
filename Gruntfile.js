fcf067fed89dfc5d8752d4afe0e9581abd98d1db
Sweeck <tsweeck@gmail.com>
2016-03-18 11:45:53 +0100

    here let's try again

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,191 +1,156 @@
-/* eslint-disable no-process-env */
 module.exports = function(grunt) {
 
+  // configure the tasks
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-
-    eslint: {
-      options: {
-      },
-      files: [
-        '*.js',
-        'bench/**/*.js',
-        'tasks/**/*.js',
-        'lib/**/!(*.min|parser).js',
-        'spec/**/!(*.amd|json2|require).js'
-      ]
-    },
-
-    clean: ['tmp', 'dist', 'lib/handlebars/compiler/parser.js'],
-
+	pkg: grunt.file.readJSON('package.json'),
+  
     copy: {
-      dist: {
-        options: {
-          processContent: function(content) {
-            return grunt.template.process('/**!\n\n @license\n <%= pkg.name %> v<%= pkg.version %>\n\n<%= grunt.file.read("LICENSE") %>\n*/\n')
-                + content;
-          }
-        },
-        files: [
-          {expand: true, cwd: 'dist/', src: ['*.js'], dest: 'dist/'}
-        ]
+      build: {
+        cwd: 'source',
+        src: [ '**', '!**/*.styl', '!**/*.coffee', '!**/*.jade' ],
+        dest: 'build',
+        expand: true
       },
-      components: {
-        files: [
-          {expand: true, cwd: 'components/', src: ['**'], dest: 'dist/components'},
-          {expand: true, cwd: 'dist/', src: ['*.js'], dest: 'dist/components'}
-        ]
       }
-    },
-
-    babel: {
-      options: {
-        sourceMaps: 'inline',
-        loose: ['es6.modules'],
-        auxiliaryCommentBefore: 'istanbul ignore next'
+      clean: {
+        build: {
+        src: [ 'build' ]
       },
-      cjs: {
-        files: [{
-          cwd: 'lib/',
-          expand: true,
-          src: '**/!(index).js',
-          dest: 'dist/cjs/'
-        }]
-      }
-    },
-    webpack: {
-      options: {
-        context: __dirname,
-        output: {
-          path: 'dist/',
-          library: 'Handlebars',
-          libraryTarget: 'umd'
-        }
+      stylesheets: {
+        src: [ 'build/**/*.css', '!build/application.css' ]
       },
-      handlebars: {
-        entry: './dist/cjs/handlebars.js',
-        output: {
-          filename: 'handlebars.js'
-        }
-      },
-      runtime: {
-        entry: './dist/cjs/handlebars.runtime.js',
-        output: {
-          filename: 'handlebars.runtime.js'
-        }
-      }
-    },
-
-    uglify: {
-      options: {
-        mangle: true,
-        compress: true,
-        preserveComments: /(?:^!|@(?:license|preserve|cc_on))/
-      },
-      dist: {
-        files: [{
-          cwd: 'dist/',
-          expand: true,
-          src: ['handlebars*.js', '!*.min.js'],
-          dest: 'dist/',
-          rename: function(dest, src) {
-            return dest + src.replace(/\.js$/, '.min.js');
-          }
-        }]
-      }
-    },
-
-    concat: {
-      tests: {
-        src: ['spec/!(require).js'],
-        dest: 'tmp/tests.js'
-      }
-    },
-
-    connect: {
-      server: {
-        options: {
-          base: '.',
-          hostname: '*',
-          port: 9999
-        }
-      }
-    },
-    'saucelabs-mocha': {
-      all: {
-        options: {
-          build: process.env.TRAVIS_JOB_ID,
-          urls: ['http://localhost:9999/spec/?headless=true'],
-          detailedError: true,
-          concurrency: 4,
-          browsers: [
-            {browserName: 'chrome'},
-            {browserName: 'firefox', platform: 'Linux'},
-            {browserName: 'safari', version: 9, platform: 'OS X 10.11'},
-            {browserName: 'safari', version: 8, platform: 'OS X 10.10'},
-            {browserName: 'internet explorer', version: 11, platform: 'Windows 8.1'},
-            {browserName: 'internet explorer', version: 10, platform: 'Windows 8'}
-          ]
-        }
-      },
-      sanity: {
-        options: {
-          build: process.env.TRAVIS_JOB_ID,
-          urls: ['http://localhost:9999/spec/umd.html?headless=true', 'http://localhost:9999/spec/umd-runtime.html?headless=true'],
-          detailedError: true,
-          concurrency: 2,
-          browsers: [
-            {browserName: 'chrome'},
-            {browserName: 'internet explorer', version: 10, platform: 'Windows 8'}
-          ]
-        }
-      }
-    },
-
-    watch: {
       scripts: {
-        options: {
-          atBegin: true
-        },
-
-        files: ['src/*', 'lib/**/*.js', 'spec/**/*.js'],
-        tasks: ['build', 'tests', 'test']
-      }
+        src: [ 'build/**/*.js', '!build/application.js' ]
+      },
+    },
+    stylus: {
+      build: {
+      options: {
+      linenos: true,
+      compress: false
+    },
+    files: [{
+      expand: true,
+      cwd: 'source',
+      src: [ '**/*.styl' ],
+      dest: 'build',
+      ext: '.css'
+    }]
     }
+    },
+    autoprefixer: {
+      build: {
+      expand: true,
+      cwd: 'build',
+      src: [ '**/*.css' ],
+      dest: 'build'
+    }
+    },
+    cssmin: {
+      build: {
+      files: {
+      'build/application.css': [ 'build/**/*.css' ]
+    }
+    }
+    },
+    coffee: {
+      build: {
+      expand: true,
+      cwd: 'source',
+      src: [ '**/*.coffee' ],
+      dest: 'build',
+      ext: '.js'
+    }
+    },
+    uglify: {
+      build: {
+      options: {
+      mangle: false
+    },
+    files: {
+      'build/application.js': [ 'build/**/*.js' ]
+    }
+    }
+    },
+      jade: {
+        compile: {
+        options: {
+        data: {}
+    },
+    files: [{
+      expand: true,
+      cwd: 'source',
+      src: [ '**/*.jade' ],
+      dest: 'build',
+      ext: '.html'
+    }]
+    }
+    },
+    watch: {
+  stylesheets: {
+    files: 'source/**/*.styl',
+    tasks: [ 'stylesheets' ]
+  },
+  scripts: {
+    files: 'source/**/*.coffee',
+    tasks: [ 'scripts' ]
+  },
+  jade: {
+    files: 'source/**/*.jade',
+    tasks: [ 'jade' ]
+  },
+  copy: {
+    files: [ 'source/**', '!source/**/*.styl', '!source/**/*.coffee', '!source/**/*.jade' ],
+    tasks: [ 'copy' ]
+  }
+
+    },
+    connect: {
+  server: {
+    options: {
+      port: 4000,
+      base: 'build',
+      hostname: '*'
+    }
+  }
+}
+
   });
 
-  // Build a new version of the library
-  this.registerTask('build', 'Builds a distributable version of the current project', [
-                    'eslint',
-                    'parser',
-                    'node',
-                    'globals']);
-
-  this.registerTask('node', ['babel:cjs']);
-  this.registerTask('globals', ['webpack']);
-  this.registerTask('tests', ['concat:tests']);
-
-  this.registerTask('release', 'Build final packages', ['eslint', 'uglify', 'test:min', 'copy:dist', 'copy:components']);
-
-  // Load tasks from npm
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-connect');
+  // load the tasks
   grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-autoprefixer');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-coffee');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jade');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-babel');
-  grunt.loadNpmTasks('grunt-eslint');
-  grunt.loadNpmTasks('grunt-saucelabs');
-  grunt.loadNpmTasks('grunt-webpack');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  // define the tasks
+  grunt.registerTask(
+  'stylesheets', 
+  'Compiles the stylesheets.', 
+  [ 'stylus', 'autoprefixer', 'cssmin', 'clean:stylesheets' ]
+  );
 
-  grunt.task.loadTasks('tasks');
+  grunt.registerTask(
+  'scripts', 
+  'Compiles the JavaScript files.', 
+  [ 'coffee', 'uglify', 'clean:scripts' ]
+  );
 
-  grunt.registerTask('bench', ['metrics']);
-  grunt.registerTask('sauce', process.env.SAUCE_USERNAME ? ['tests', 'connect', 'saucelabs-mocha'] : []);
+  grunt.registerTask(
+  'build', 
+  'Compiles all of the assets and copies the files to the build directory.', 
+  [ 'clean:build', 'copy', 'stylesheets', 'scripts', 'jade' ]
+  );
 
-  grunt.registerTask('travis', process.env.PUBLISH ? ['default', 'sauce', 'metrics', 'publish:latest'] : ['default']);
+  grunt.registerTask(
+  'default', 
+  'Watches the project for changes, automatically builds them and runs a server.', 
+  [ 'build', 'connect', 'watch' ]
+  );
 
-  grunt.registerTask('dev', ['clean', 'connect', 'watch']);
-  grunt.registerTask('default', ['clean', 'build', 'test', 'release']);
+  
 };
